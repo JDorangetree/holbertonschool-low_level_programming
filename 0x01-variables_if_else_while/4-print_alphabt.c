c3bd9084375a0d8d02cc66e77f2878b5e6bbb87b
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-19 21:49:52 +0000

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going print us the alphabet in lowercase
+ * except "e" and "q"
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		if((n !='e') && (n != 'q'))
+		{
+			putchar(n);
+		}
+		{
+			n++;
+		}
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
