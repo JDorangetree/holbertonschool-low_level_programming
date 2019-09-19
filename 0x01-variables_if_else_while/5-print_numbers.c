acc32827ab25119fa1e9970bf579df15d44ae3b5
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-19 22:18:37 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going print us all single digit numbers of base 10
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	{
+		printf("\n");
+	}
+	return (0);
+}
