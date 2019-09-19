b95a4ee829e10c708e8be3d494c19a29a59a6116
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-19 22:54:04 +0000

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going print us all single digit numbers of base 10
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
