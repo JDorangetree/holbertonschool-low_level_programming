6ba1d06acff5513a81833f2154b5f9be0198b8e0
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-19 20:46:00 +0000

    origin master

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going print us the alphabet in lowercase
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	char n = 'A';
+
+	while (n <= 'Z')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'a';
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
