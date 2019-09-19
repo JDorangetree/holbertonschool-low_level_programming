9923bc2c1228d3526edb9e0d05d4c38ab3d9b0e7
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-19 22:35:54 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going print us all single digit numbers of base 10
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <=57; n++)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
