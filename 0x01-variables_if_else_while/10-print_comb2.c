a5bbca12ccb130c54281b590d9741133167006a8
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-20 17:04:07 +0000

    print_comb

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This is going print us all single digit numbers of base 10
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	int n;
+	int l;
+
+	n = 48;
+	while (n <= 57)
+	{
+		l = 48;
+		while (l <= 57)
+		{
+			/**putchar(n);
+			putchar(l);
+			*/
+			if (n != 57)
+			{
+				putchar(n);
+				putchar(l);
+				putchar(44);
+				putchar(32);
+			}
+			else
+			{
+				if(l != 57)
+				{
+					putchar(n);
+					putchar(l);
+					putchar(44);
+					putchar(32);
+				}
+				else
+				{
+					putchar(n);
+					putchar(l);
+				}
+			}
+			l++;
+		}
+		n++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
