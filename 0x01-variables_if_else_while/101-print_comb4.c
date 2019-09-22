55ee7fd6197ddac0f2faee16cc546e322f01f1b0
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-22 18:56:54 +0000

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - Accept empty value
+ * write - Screen information
+ * @void: Description of a empty parameter
+ * Description: This is going print us all numbers from 0 to 99
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int j;
+	int x;
+	int y;
+
+	m = 48;
+	x = 8;
+	while (m <= 55)
+	{
+		n = (57 - x);
+		y = 7;
+		while (n <= 56)
+		{
+			j = (57 - y);
+			while (j <= 57)
+			{
+				putchar(m);
+				putchar(n);
+				putchar(j);
+				if (m < 55 || n != 56 || j != 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				j++;
+			}
+			n++;
+			y--;
+		}
+		m++;
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
