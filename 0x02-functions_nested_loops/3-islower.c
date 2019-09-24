3e584819491b0bfe23081c8549338661ed63a3dd
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-24 19:41:21 +0000

    islower3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 /**
  * _islower(int c) -  Determine if a character is lowercase.
  * @c: The evaluated letter
- *
- * Description: This prints the alphabet 10 times
+ * Description: This prints 1 if character is lowercase
  * Return: 0 Value if it works
  */
 int _islower(int c)
