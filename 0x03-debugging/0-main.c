12136ec521685072076d2d02acebf9dce68276ef
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-25 20:11:59 +0000

    upload files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
- * @void: description of a empty parameter
- * Description: Tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -12,5 +11,6 @@ int main(void)
 
         i = 0;
         positive_or_negative(i);
-	return (0);
+
+        return (0);
 }
