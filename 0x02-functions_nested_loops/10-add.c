61a156e155dd9b5116f29722a8a001b518d9cb45
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-25 15:25:53 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * jack_bauer -  Print every second and minute of the day.
+ * Description: This prints absolute value
+ * Return: Value of the last digit
+ */
+int add(int x, int y)
+{
+	int suma;
+
+	suma = x + y;
+	return(suma);
+}
