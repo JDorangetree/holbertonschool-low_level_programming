f17a4b2dc9a33a9cc8ca35db12a52c44ba69e308
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-26 20:55:30 +0000

    print line2

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 /**
  * print_line - Draws a straight line
  *
- *@n: numbers of times the character
+ * @n: number of times the characters
  * Description: This draws a stright line
  * Return: 0 Value if it works
  */
@@ -10,7 +10,7 @@ void print_line(int n)
 {
 	if (n > 0)
 	{
-		while (n >= 0)
+		while (n > 0)
 		{
 			_putchar(95);
 			n--;
