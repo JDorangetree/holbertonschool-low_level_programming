ef6d53ef6e409038036979b37ec1ec46dfd84808
JDorangetree <julian.naranjo2014@gmail.com>
2019-09-26 18:19:42 +0000

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - Print the numbers from 0 to 9
+ *
+ *@void: print numbers
+ * Description: This prints number form 0 to 9
+ * Return: 0 Value if it works
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 48;
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
