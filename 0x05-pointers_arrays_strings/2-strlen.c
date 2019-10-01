dca05101e5cad3ec7bb5b8be360376af44cf960e
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-01 15:48:56 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * swap_int -  swap two integers.
+ *@a: first int to swap
+ *@b: secod int to swap
+ * Description: Swap the values of two integers
+ * Return: return the result
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return(i);
+}
