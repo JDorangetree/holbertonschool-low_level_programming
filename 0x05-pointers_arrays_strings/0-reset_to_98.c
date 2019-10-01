90fbdd525bb26a2fdad1b0226d8e5ca81d817df9
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-01 13:01:48 +0000

    upload task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Updates the value it points to to 98
+ *
+ *@n: pointer
+ * Description: This prints number form 0 to 14 10 times
+ * Return: 0 Value if it works
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int -  multiplies two integers.
+ *@a: first int to multiplies
+ *@b: secod int to multiplies
+ * Description: This multiplies two integers
+ * Return: return the result
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
