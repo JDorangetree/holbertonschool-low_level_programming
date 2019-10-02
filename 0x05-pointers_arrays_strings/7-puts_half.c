257317c4eea91ee6907e94df3f0f65226d8ee1ed
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-02 03:40:22 +0000

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * puts_half -  print pair number of a string
+ *@str: string for print
+ * Description: return a string
+ * Return: return the result
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	n = 0;
+	if (i % 2 == 0)
+	{
+		n = (i / 2);
+		while (n < i)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = ((i - 1) / 2);
+		while (n < i)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
