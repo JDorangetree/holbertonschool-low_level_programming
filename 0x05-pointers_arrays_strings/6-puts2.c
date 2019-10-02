c9f3d8976d4e996380923ea7504bc0da99d24601
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-02 02:06:26 +0000

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * puts2 -  print pair number of a string
+ *@str: string for print
+ * Description: return a string
+ * Return: return the result
+ */
+void puts2(char *str)
+{
+	int i;
+	int n;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	n = 0;
+	while (i >= 0)
+	{
+		if (str[n] % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+		n++;
+		i--;
+	}
+	_putchar('\n');
+}
