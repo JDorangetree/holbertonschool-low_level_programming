29c43489defe7129eeadd864f9f3b086d1d7b1f3
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-03 20:24:39 +0000

    strcmp2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,5 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i > j)
-	{
-		return (15);
-	}
-	if (i < j)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (*s1 - *s2);
 }
