c6b58fa37ec8598c2310ae7cbea7184341c73a5a
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-04 16:28:31 +0000

    rot

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+**
+ *rot13 - string to rot13
+*@s: string
+*Return: s
+ */
+char *rot13(char *s)
+{
+	int i = 0, c;
+
+	while (s[i] != '\0')
+	{
+		c = 0;
+		if  (c == 0 && ((s[i] >= 97 && s[i] <= 109) || (s[i] >= 65 && s[i] <= 77)))
+		{
+			s[i] = s[i] + 13;
+			c++;
+		}
+		while (c == 0 && ((s[i] >= 110 && s[i] <= 122) || (s[i] > 77 && s[i] < 91)))
+		{
+			s[i] = s[i] - 13;
+		}
+		i++;
+	}
+	return (s);
+}
