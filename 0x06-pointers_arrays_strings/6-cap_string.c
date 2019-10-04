c137a0559ee994a55a4b00d5b979263c9b540f50
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-04 17:55:12 +0000

    fix 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * cap_string - compare two string
  * @s: array to upper
@@ -10,17 +11,16 @@ char *cap_string(char *s)
 {
 	int n;
 	int se;
-	int separator[] = {32, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 9};
-
+	char separa[] = {32, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 9, 0};
 
 	n = 0;
 	while (s[n] != '\0')
 	{
-		if ((s[n] >= 97 && s[n] <= 122))
+		if (s[n] >= 97 && s[n] <= 122)
 		{
-			for (se = 0; se <= 12; se++)
+			for (se = 0; se <= 13; se++)
 			{
-				if (separator[se] == s[n - 1])
+				if (separa[se] == s[n - 1])
 				{
 					s[n] = s[n] - 32;
 				}
