51d10b97b7ca0e73479226d3a42dc79d788ac4a2
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-04 16:10:19 +0000

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,30 +4,27 @@
  * @s: array to upper
  *
  * Description: reverses the content of an array
- * Return: array upper
+ * Return: s
  */
 char *cap_string(char *s)
 {
 	int n;
 	int se;
-	int separator[] = {32, 10, 9, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int separator[] = {32, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 9};
 
 
 	n = 0;
-	if (s[n] >= 97 && s[n] <= 122)
-	{
-		s[n] = s[n] - 32;
-	}
 	while (s[n] != '\0')
 	{
-		se = 0;
-		while (se <= 12)
+		if((s[n] >= 97 && s[n] <= 122))
 		{
-			if((s[n - 1] == separator[se]) && (s[n] >= 97 && s[n] <= 122))
+			for (se = 0; se <= 12; se++)
 			{
-				s[n] = s[n] - 32;
+				if (separator[se] == s[n - 1])
+				{
+					s[n] = s[n] - 32;
+				}
 			}
-			se++;
 		}
 		n++;
 	}
