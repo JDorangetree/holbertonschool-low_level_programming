d5b183d3a9d50931fc21c49649d927779ad02ecf
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-07 20:57:54 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _memset - compare two string
+ * @b: new valor
+ * @n: pisition
+ * @s: pointer to buffer
+ *
+ * Description: fill memory with a constant byte
+ * Return: s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i <= n)
+	{
+		*dest = src[i];
+		i++;
+		dest++;
+	}
+	return (dest);
+}
