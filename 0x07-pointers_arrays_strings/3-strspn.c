a240b45e1351758815b2e4d0e1c511a6b3840415
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-08 02:52:58 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - compare two string
+ * @s: new valor
+ * @accept: pisition
+ *
+ * Description: fill memory with a constant byte
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+	j = 0;
+	while (j <= i)
+	{
+		if (s[j] == accept[0])
+		{
+			return (j + 1);
+		}
+		j++;
+	}
+	return (0);
+}
