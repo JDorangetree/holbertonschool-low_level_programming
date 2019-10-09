e94c9fbffe8645822e01497c28723a2a14142e70
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-09 17:50:32 +0000

    first task *

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,16 @@
 #include "holberton.h"
 /**
- * _memset - compare two string
- * @b: new valor
- * @n: pisition
- * @s: pointer to buffer
+ * _puts_recursion - print a string
+ * @s: pointer to char
  *
- * Description: fill memory with a constant byte
- * Return: s
+ * Description: print a string followed by a new line
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar (*s);
-		_puts_recursion (s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
