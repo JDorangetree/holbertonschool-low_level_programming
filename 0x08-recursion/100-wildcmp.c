b85b17fd8df40262fa0d8cfc943776d69dc0187e
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-10 04:10:08 +0000

    prototype

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _divisor - Returns 1 if the input integer is a prime number
+ * @s1: First input number
+ * @s2: Second input divisor
+ *
+ * Description: Returns 1 if the input integer is a prime number
+ * Return: Returns 1 if is a prime number 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (s1);
+}
