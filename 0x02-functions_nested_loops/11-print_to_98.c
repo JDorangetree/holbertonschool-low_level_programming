0a5f5332ecda15ad4ee5dc524e9cac86dba73bb1
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-11 14:51:18 +0000

    mandatory task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_to_98 -  adds two integers.
+ * print_to_98 -  print all naturals number from n to 98.
  *@n: from to count
  * Description: print all natural number from n to 98
- * Return: return the result
  */
 void print_to_98(int n)
 {
@@ -18,19 +17,16 @@ void print_to_98(int n)
 		}
 		printf("98\n");
 	}
+	else if (n > 98)
+	{
+		for (cont = n; cont > 98; cont--)
+		{
+			printf("%d, ", cont);
+		}
+		printf("98\n");
+	}
 	else
 	{
-		if (n > 98)
-		{
-			for (cont = n; cont > 98; cont--)
-			{
-				printf("%d, ", cont);
-			}
-			printf("98\n");
-		}
-		else
-		{
-			printf("98\n");
-		}
+		printf("98\n");
 	}
 }
