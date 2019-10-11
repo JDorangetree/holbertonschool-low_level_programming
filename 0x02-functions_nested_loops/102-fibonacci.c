f08b697429db1663971f1be860d7bfc88d1ec2ef
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-11 20:42:01 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Prints the 9 times table, starting with 0
+ * @void: the evaluated input
+ *
+ * Description: Prints the n times table, starting with 0
+ * Return: 0 value
+ */
+int main(void)
+{
+	long int count;
+	long int num_fib;
+	long int prenum_fib;
+	long int src;
+
+	num_fib = 2;
+	count = 56;
+	prenum_fib = 1;
+	while (count > 1)
+	{
+		if (num_fib <= 2)
+		{
+			printf("%li, ", prenum_fib);
+			printf("%li, ", num_fib);
+		}
+		src = num_fib;
+		num_fib = num_fib + prenum_fib;
+		printf("%li, ", num_fib);
+		prenum_fib = src;
+		count--;
+	}
+	num_fib = num_fib + prenum_fib;
+	printf("%li, ", num_fib);
+	return (0);
+}
