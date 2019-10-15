f2bbc833683a6728984155e26ea6f8b0b28f85bc
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-15 17:27:55 +0000

    create array 2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print name of file
- * @argc: Firts input int argument
- * @argv: Second input char argument of string
+ * create_array - print name of file
+ * @size: size of array
+ * @c: character
  *
- * Description: print its name of executable file
- * Return: 0 if it works
+ * Description: create an array of char
+ * Return: a pointer to the array
  */
 char *create_array(unsigned int size, char c)
 {
@@ -17,12 +17,12 @@ char *create_array(unsigned int size, char c)
 	{
 		return (0);
 	}
-	str = malloc(sizeof (char) * size);
+	str = malloc(sizeof(char) * size);
 	i = 0;
 	while (i < size)
 	{
 		str[i] = c;
 		i++;
 	}
-	return(str);
+	return (str);
 }
