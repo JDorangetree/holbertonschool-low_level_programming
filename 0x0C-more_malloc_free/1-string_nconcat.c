0c199d09855eb66c72a631dee50f8373a4b5bbc9
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-17 19:49:01 +0000

    string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - allocates memory using
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ *
+ * Description: allocates memory using
+ * Return: a pointer to the array
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str, *null;
+	unsigned int i, j, count;
+
+	null = "";
+
+	if (s1 == NULL)
+		s1 = null;
+	if (s2 == NULL)
+		s2 = null;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
+
+	if (n >= j)
+		str = malloc(sizeof(char) * (i + j + 1));
+	else
+		str = malloc(sizeof(char) + (i + n + 1));
+	if (!str)
+		return (NULL);
+	if (n < j)
+	{
+		for (count = 0; count < i; count++)
+			str[count] = s1[count];
+		for (count = 0; count < n; i++, count++)
+			str[i] = s2[count];
+	}
+	else
+	{
+		for (count = 0; count < i; count++)
+			str[count] = s1[count];
+		for (count = 0; count < j; i++, count++)
+			str[i] = s2[count];
+	}
+	return (str);
+}
