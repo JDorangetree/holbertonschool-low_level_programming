75f589dd928432fd6fce22d00bf9c82dc3af44d6
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-21 19:37:54 +0000

    like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,21 +1,5 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H
-#define _3_FUNCTION_LIKE_MACRO_H
-/**
- * ABS -  Determine the absolute value of an integer.
- * @n: The evaluated number
- * Description: This prints absolute value
- * Return: 1 Value if it lowercase
- */
-int ABS(int n)
-{
+#ifndef ABS_H
+#define ABS_H
+#define ABS(x) (((x) < 0) ? ((x) * -1) : (x))
 
-	if (n < 0)
-	{
-		return (n * (-1));
-	}
-	else
-	{
-		return (n);
-	}
-}
 #endif
