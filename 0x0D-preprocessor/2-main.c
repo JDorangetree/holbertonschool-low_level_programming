1ad0619f4f80463c1c48231125ac84b58a51b2bd
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-21 18:00:21 +0000

    upload task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - name of the current input file
+ * write - Screen information
+ * @void: Description of a empty parameter
+ *
+ * Description: This is going give us if a random is positive or negative
+ * Return: 0 Value if it works
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
