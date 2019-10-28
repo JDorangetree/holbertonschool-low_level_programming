bb1c81e957ac354573351dc84f98e901afa77c34
JDorangetree <julian.naranjo2014@gmail.com>
2019-10-28 01:15:34 +0000

    fix case of separator is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,27 +16,24 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 
-	if (separator)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		str = va_arg(valist, char*);
+		if (str)
 		{
-			str = va_arg(valist, char*);
-			if (str)
-			{
-				if (!(n - i == 1))
-					printf("%s%s", str, separator);
-				else
-					printf("%s", str);
-			}
+			if (!(n - i == 1) && separator)
+				printf("%s%s", str, separator);
 			else
-			{
-				if (!(n - i == 1))
-					printf("(nil)%s", separator);
-				else
-					printf("(nil)");
-			}
+				printf("%s", str);
 		}
-		putchar('\n');
+		else
+		{
+			if (!(n - i == 1) && separator)
+				printf("(nil)%s", separator);
+			else
+				printf("(nil)");
+		}
 	}
+	putchar('\n');
 	va_end(valist);
 }
