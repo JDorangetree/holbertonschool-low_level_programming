3f0353e42c226828ff08eebe9ba26f23c1969233
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-05 02:06:44 +0000

    upload task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints sums of diag
+ * @a: pointer to multidimensinal array
+ * @size: size of array
+ * Description: Locates a substring
+ * Return: char
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum_diag1, sum_diag2;
+
+	i = 0;
+	sum_diag1 = 0;
+	sum_diag2 = 0;
+	while (i < (size * size))
+	{
+		sum_diag1 = sum_diag1 + a[i];
+		i = i + size + 1;
+	}
+	i = 0;
+	while (i < (size * size) - 1)
+	{
+		sum_diag2 = sum_diag2 + a[i];
+		i = i + size - 1;
+	}
+	printf("%d , %d\n", sum_diag1, sum_diag2);
+}
