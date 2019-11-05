c2e178232596a4b956c6c8f6e8176de293eff0be
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-05 20:37:25 +0000

    upload task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 /**
  * new_dog - Creates a new dog
  * @name: String name
@@ -12,41 +12,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int i = 0;
-	char *nam, *own;
+	dog_t *rex;
+	char *new_name, *new_owner;
+	int i, j, x;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	rex = malloc(sizeof(dog_t));
+	if (rex == NULL)
 		return (NULL);
-	while (name[i] != 0)
-		i++;
-	nam = malloc(sizeof(char) * (i + 1));
-	if (nam  == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
 	i = 0;
-	while (owner[i] != 0)
+	while (name[i] != '\0')
 		i++;
-	own = malloc(sizeof(char) * (i + 1));
-	if (own == NULL)
+	new_name = malloc(sizeof(char) * (i + 1));
+	if (new_name == NULL)
 	{
-		free(nam);
-		free(dog);
+		free(rex);
 		return (NULL);
 	}
-	for (i = 0; name[i] != 0; i++)
-		nam[i] = name[i];
-	nam[i] = 0;
-	for (i = 0; owner[i] != 0; i++)
-		own[i] = owner[i];
-	own[i] = 0;
-	dog->name = nam;
-	dog->age = age;
-	dog->owner = own;
-	return (dog);
+	j = 0;
+	while (owner[j] != '\0')
+		j++;
+	new_owner = malloc(sizeof(char) * (j + 1));
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(rex);
+		return (NULL);
+	}
+	for (x = 0; x <= i; x++)
+		new_name[x] = name[x];
+	new_name[x] = '\0';
+	for (x = 0; x <= j; x++)
+		new_owner[x] = owner[x];
+	new_owner[x] = '\0';
+	(*rex).name = new_name;
+	(*rex).age = age;
+	(*rex).owner = new_owner;
+	return (rex);
 }
