074b866959e714bda8bea5ee58b291c64e077396
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-05 22:36:32 +0000

    [Fix] my style2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,17 +13,16 @@ char *str_concat(char *s1, char *s2)
 	int i, j, x, y;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		ptr = "";
-	else
-	{
-		i = 0;
-		while (s1[i] != '\0')
-			i++;
-		j = 0;
-		while (s2[j] != '\0')
-			j++;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	j = 0;
+	while (s2[j] != '\0')
+		j++;
 	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (!ptr)
 		return (NULL);
