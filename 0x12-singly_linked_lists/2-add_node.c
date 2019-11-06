7b86484e8eee2585abf8afe7f34598d28c140b2c
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-06 20:23:49 +0000

    [Fix] replace strlen function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
 
 	new_node = ((list_t *)malloc(sizeof(list_t)));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	new_node->len = i;
 	if (head == NULL)
 	{
 		new_node->next = NULL;
