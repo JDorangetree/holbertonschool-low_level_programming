46e4291c1a9d92de5d74265579df056411c0c247
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-06 22:35:48 +0000

    [Feature] upload task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - adds a new node at the beginning
+ * @head: Head
+ *
+ * Description: adds a new node at the beginning
+ * of a list_t list.
+ * Return: The number of nodes
+ */
+void free_list(list_t *head)
+{
+	list_t *iterator_next;
+
+	iterator_next = head;
+	while (iterator_next->next != NULL)
+	{
+		iterator_next = head;
+		free(iterator_next->str);
+		head = iterator_next->next;
+		free(iterator_next);
+	}
+	free(head);
+}
