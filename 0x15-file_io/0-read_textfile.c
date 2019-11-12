a758e5f5a2c42d9a25f0322b9f14861f6841a611
JDorangetree <julian.naranjo2014@gmail.com>
2019-11-12 20:09:17 +0000

    upload task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * read_textfile - Read a text file
+ * @filename: header of the linked list to print
+ * @letters: header of the linked list to print
+ * Return: number of nodes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, reader, writer;
+	char *buffer;
+
+	fd = open(filename, O_RDONLY);
+
+	if ((fd == -1) | (filename == NULL))
+	{
+		return (0);
+	}
+
+	buffer = malloc(letters + 1);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	buffer[letters] = '\0';
+
+	reader = read(fd, buffer, letters);
+	if (reader == -1)
+	{
+		return (0);
+		free(buffer);
+	}
+	writer = write(1, buffer, reader);
+	if (writer == -1)
+	{
+		return (0);
+		free(buffer);
+	}
+	free(buffer);
+	close(fd);
+	return (writer);
+}
