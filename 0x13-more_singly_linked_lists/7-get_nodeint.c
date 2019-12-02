6fe595af43ca1f9972c188ee9642f35ae90af024
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-02 19:59:17 +0000

    [Fix] betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,38 +1,30 @@
 #include "lists.h"
 /**
- * pop_listint - deletes the head
+ * get_nodeint_at_index - deletes the head
  * @head: head
+ * @index: index of a node
  *
- * Description: Deletes the head node of a
- * listint_t linked list, and returns the
- * head node’s data (data).
- * Return: data of delete node
+ * Description: return the node of a
+ * listint_t linked list, according to an index
+ * Return: node
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node_iterator;
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    if(!head)
-        return(NULL);
-    node_iterator = head;
-    while (head)
-    {
-        head = head->next;
-        count++;
-    }
-    if (index <= count)
-    {
-        while(index > 0)
-        {
-            node_iterator = node_iterator->next;
-            index--;
-        }
-        return(node_iterator);
-    }
-    else
-    {
-        node_iterator = NULL;
-        return(node_iterator);
-    }
+	if (!head)
+		return (NULL);
+	node_iterator = head;
+	while (head)
+	{
+		head = head->next;
+		count++;
+	}
+	while (index > 0)
+	{
+		node_iterator = node_iterator->next;
+		index--;
+	}
+	return (node_iterator);
 }
