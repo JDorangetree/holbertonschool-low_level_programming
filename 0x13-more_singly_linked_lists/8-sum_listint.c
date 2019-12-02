63000fd559ff92d40190f12651ed954de4b6cb3e
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-02 21:57:10 +0000

    [Feature] sum listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_listint - deletes the head
+ * @head: head
+ *
+ * Description: return the sum of a integer of
+ *  a listint_t linked list
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *node_iterator;
+	int sum;
+
+	node_iterator = head;
+	while (head)
+	{
+		sum = sum + node_iterator->n;
+		head = head->next;
+		node_iterator = head;
+	}
+	return (sum);
+}
