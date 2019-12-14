20d513e6cdac38901e71334f94704e64c159fe59
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 16:58:24 -0500

    [Feature] upload task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a dlistint_t list.
+ * @head: pointer to header of the linked list to print
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+	{
+		while (head->next != NULL)
+		{
+			head = head->next;
+		}
+		while (head->prev)
+		{
+			tmp = head;
+			head = tmp->prev;
+			free(tmp);
+		}
+		free(head);
+	}
+}
