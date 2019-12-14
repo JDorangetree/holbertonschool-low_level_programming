b388a82b08917c85fe87de3c7110e1bbe19c02ed
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 15:55:58 -0500

    [Fix] optimization

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,17 +14,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
 	if (*head == NULL)
 	{
 		new_node->next = NULL;
-		new_node->prev = NULL;
 		*head = new_node;
 	}
 	else
 	{
 		(*head)->prev = new_node;
 		new_node->next = *head;
-		new_node->prev = NULL;
 		*head = new_node;
 	}
 	return (new_node);
