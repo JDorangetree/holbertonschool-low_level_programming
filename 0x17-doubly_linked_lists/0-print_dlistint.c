e3c15cc786d28650602831fcded9a0125caf4e8d
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 13:25:26 -0500

    [Fix] case does not exist h

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,16 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	while (h->next != NULL)
+	if (h)
 	{
+		while (h->next != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			i++;
+		}
 		printf("%d\n", h->n);
-		h = h->next;
 		i++;
 	}
-	printf("%d\n", h->n);
-	i++;
 	return (i);
 }
