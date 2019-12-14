4bb51cc40d6385565c51d4fac32cdf09bb59e6c5
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 18:46:29 -0500

    [Fix] my style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 /**
- * free_dlistint - free a dlistint_t list.
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
  * @head: pointer to header of the linked list to print
+ * @index: index to return node
  *
+ * Return: Return: address to node or NULL if index does not exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *tmp;
+
 	if (!head)
 		return (NULL);
 	while (index > 0)
 	{
 		head = head->next;
-		if (head->next == NULL)
+		tmp = head;
+		tmp = tmp->next;
+		if (tmp == NULL)
 			return (NULL);
 		index--;
 	}
