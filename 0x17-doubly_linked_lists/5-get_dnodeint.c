00d8ceb2a774b2d42d83f0abe5f177a19998d977
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 18:38:08 -0500

    [Feature] betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,32 +1,19 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * free_dlistint - free a dlistint_t list.
  * @head: pointer to header of the linked list to print
- * @index: index to return node
  *
- * Return: Return: address to node or NULL if index does not exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *tmp;
-	unsigned int i;
-
 	if (!head)
 		return (NULL);
-	tmp = head;
-	i = 0;
-	while (tmp)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-	if (index > (i - 1))
-		return (NULL);
-	i = 0;
-	while (i < index)
+	while (index > 0)
 	{
 		head = head->next;
-		i++;
+		if (head->next == NULL)
+			return (NULL);
+		index--;
 	}
 	return (head);
 }
