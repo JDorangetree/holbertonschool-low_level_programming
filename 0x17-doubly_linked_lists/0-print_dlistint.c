5623ab2d4102a96b2ca4e062a8dac5ae81dd2637
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 12:12:21 -0500

    [Feature] upload task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - Print a list_t structure
+ * @h: header of the linked list to print
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t i = 0;
+
+    while(h->next != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    printf("%d\n", h->n);
+    i++;
+    return(i);
+}
