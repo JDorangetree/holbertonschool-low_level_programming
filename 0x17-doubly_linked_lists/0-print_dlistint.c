366d85fdf26dc93b8cc931dfa1c463a5d1b720d1
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-14 13:17:49 -0500

    [Feature] betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 /**
- * print_list - Print a list_t structure
+ * print_dlistint - Print a dlistint_t structure
  * @h: header of the linked list to print
  *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while(h->next != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        i++;
-    }
-    printf("%d\n", h->n);
-    i++;
-    return(i);
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	printf("%d\n", h->n);
+	i++;
+	return (i);
 }
