c5a0aaf05b8106254c2d343c5da1309fd1f29ad5
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-15 23:25:51 -0500

    [Fix] Betty style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,19 @@
 #include "lists.h"
 /**
+ * new_malloc - inserts a new node at a given position.
+ *
+ * Return: the sum of all node data
+ */
+dlistint_t *new_malloc()
+{
+	dlistint_t *node;
+
+	node = ((dlistint_t *)malloc(sizeof(dlistint_t)));
+	if (node == NULL)
+		return (NULL);
+	return (node);
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: double pointer to header of the linked list
  * @idx: index of the node that should be added
@@ -14,31 +28,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!h)
 		return (NULL);
-	new_node = ((dlistint_t *)malloc(sizeof(dlistint_t)));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n, tmp = *h;
+	new_node = new_malloc(), new_node->n = n, tmp = *h;
 	while (tmp)
 		tmp = tmp->next, i++;
-	iterator_node = *h;
+	iterator_node = *h, new_node->prev = NULL;
+	if (idx == 0 && i == 0)
+	{
+		new_node->next = NULL, *h = new_node;
+		return (new_node);
+	}
 	if (idx < i)
 	{
 		if (idx == 0)
 		{
-			new_node->prev = NULL, new_node->next = *h;
+			new_node->next = *h;
 			if (*h)
 				(*h)->prev = new_node;
 			*h = new_node;
-		}
-		else
+		} else
 		{
 			while (idx > 1)
 				iterator_node = iterator_node->next, idx--;
 			tmp = iterator_node->next, iterator_node->next = new_node;
-			new_node->next = tmp, tmp->prev = new_node;
-			new_node->prev = iterator_node;
-		}
-	} i--;
+			new_node->next = tmp, tmp->prev = new_node, new_node->prev = iterator_node;
+		} } i--;
 	if (idx - i == 1)
 	{
 		while (i > 0)
