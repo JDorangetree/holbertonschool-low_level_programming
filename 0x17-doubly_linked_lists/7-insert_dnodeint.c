e9205f7c4c4086686e135e34219dfe07924de9cd
JDorangetree <julian.naranjo2014@gmail.com>
2019-12-15 22:08:57 -0500

    [Fix] if the head does not exist

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp, *new_node, *iterator_node;
 	unsigned int i = 0;
 
+	if (!h)
+		return (NULL);
 	new_node = ((dlistint_t *)malloc(sizeof(dlistint_t)));
 	if (new_node == NULL)
 		return (NULL);
