d8d453a93586fce09d2400b5ff4233d2c25b3ed8
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-18 15:45:09 -0500

    [Fix] does no exist ht

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,9 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	char *key_value = NULL;
 	unsigned long int index_table;
-	hash_node_t *tmp;
+	hash_node_t *tmp = NULL;
 
-	if (!key || key == NULL)
+	if (!key || key == NULL || !ht)
 		return (NULL);
 	index_table = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index_table];
