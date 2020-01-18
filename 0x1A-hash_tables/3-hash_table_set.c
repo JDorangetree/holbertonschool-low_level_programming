6f1705327cc93410127a5005323e07151733effc
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-18 14:20:51 -0500

    [Feature] upload task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to set
+ * @key: key
+ * @value: value for key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index_table;
+	hash_node_t *tmp, *new_node;
+	char *copy_value, *copy_key;
+
+	if (!key || !value)
+		return (0);
+
+	copy_value = strdup(value);
+	copy_key = strdup(key);
+
+
+	index_table = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index_table];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = copy_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	tmp = ht->array[index_table];
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(copy_value);
+		free(copy_key);
+		return (0);
+	}
+	new_node->value = copy_value;
+	new_node->key = copy_key;
+	new_node->next = tmp;
+	ht->array[index_table] = new_node;
+	return (1);
+}
