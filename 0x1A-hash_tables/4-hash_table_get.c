8070b67fbeca82822f0288ec574c026a480decae
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-18 15:35:26 -0500

    [Feature] upload task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a key/value
+ * @ht: hash table to set
+ * @key: key
+ *
+ * Return: a value associated with a key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *key_value = NULL;
+	unsigned long int index_table;
+	hash_node_t *tmp;
+
+	if (!key || key == NULL)
+		return (NULL);
+	index_table = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index_table];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			key_value = strdup(tmp->value);
+		tmp = tmp->next;
+	}
+	return (key_value);
+}
