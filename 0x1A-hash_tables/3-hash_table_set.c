b49aeff040f2202948a0146852a8b20fd2402c91
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-18 14:31:28 -0500

    [Fix] if not exist hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp->value);
 			tmp->value = copy_value;
 			return (1);
 		}
