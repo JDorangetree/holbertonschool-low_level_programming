844195b9058fd639b22c78c5c9163681982072d3
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-18 20:20:07 -0500

    [Feature] upload task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints all elements of a hash table
+ * @ht: hash table to set
+ *
+ * Return: --
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index_table = 0, number_of_elements = 0;
+	hash_node_t *tmp;
+
+	printf("{");
+	while (index_table < ht->size)
+	{
+		tmp = ht->array[index_table];
+		while (tmp)
+		{
+			if (number_of_elements != 0)
+			printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			number_of_elements++;
+		}
+		index_table++;
+	}
+	printf("}\n");
+}
