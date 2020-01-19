72ae55e008ecf66de7f777f0f0e0a80654411260
JDorangetree <julian.naranjo2014@gmail.com>
2020-01-19 14:36:15 -0500

    [Fix] memory managment

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
+			free(tmp->value);
+			free(copy_key);
 			tmp->value = copy_value;
 			return (1);
 		}
